14228c9fdb849fa5d80eb258fb6ca0c04fc3c89d
rdramos1 <ricardodaviramos.figueirefo@gmail.com>
2025-03-01 00:51:38 -0300

    Criando a classe posição xadrez

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -5,11 +5,14 @@ using xadrez;
 namespace Xadrez_console {
     class Program {
         static void Main(string[] args) {
-            Board board = new Board(8, 8);
+            /*Board board = new Board(8, 8);
 
             board.PutPart(new Tower(board, Color.Black), new Position(0, 0));
 
-            Screen.printboard(board);
+            Screen.printboard(board); */
+
+            xadrezPosition xadrezPosition = new xadrezPosition('c', 7);
+            Console.WriteLine(xadrezPosition);
 
         }
     }
---
+++ Xadrez-console/xadrez/xadrezPosition.cs
@@ -0,0 +1,17 @@
+﻿namespace xadrez {
+    class xadrezPosition {
+
+        public char row { get; set; }
+        public int line { get; set; }   
+
+        public xadrezPosition(char row, int line) {
+            this.row = row;
+            this.line = line;
+        }
+
+        override public string ToString() {
+            return "" + row + line;
+        }
+
+    }
+}
