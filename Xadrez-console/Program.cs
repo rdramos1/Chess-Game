a2ef3230963e722646bfde27fe3dcbdbdfd71a4d
rdramos1 <ricardodaviramos.figueirefo@gmail.com>
2025-03-01 00:16:22 -0300

    Criado a classe posição

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+
+namespace Xadrez_console {
+    class Program {
+        static void Main(string[] args) {
+            Position P;
+            P = new Position(3, 4);
+            Console.WriteLine("Position: " + P);
+        }
+    }
+}
---
+++ Xadrez-console/tabuleiro/Position.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuleiro {
+    class Position {
+
+        public int line { get; set; }
+        public int row { get; set; }
+
+        public Position(int line, int row) {
+            this.line = line;
+            this.row = row;
+        }
+
+        public override string ToString() {
+            return line + ", " + row; 
+        }
+
+    }
+}
